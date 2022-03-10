db0b848acdbccf6d33c440c785930e844da4c0c2
Amine <japnoo@gmail.com>
2022-03-11 00:12:58 +0100

    Fixed matching algorithm (now fully working)

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,35 +2,38 @@ import { useState } from "react";
 
 function Form(props) {
     const [guess, setGuess] = useState("");
+    const [guessCount, setGuessCount] = useState(0);
     const [result, setResult] = useState("");
 
 
     // Find which letters match the guess
     function compareLetters() {
-        let res = "";
+        let currentGuess = [props.word.length+1];
+        console.log(`test ${currentGuess}`);
         let tempWord = props.word;
         for (let i = 0; i < props.word.length; i++) {
+            // Checks if letter is in the right position
             if (props.word[i] === guess[i]) {
-                res += guess[i];
+                currentGuess[i] = guess[i];
                 tempWord = tempWord.replace(guess[i], "-");
-
             } 
-            else {
-
+        }
+        for (let i = 0; i < props.word.length; i++) {
+            if (props.word[i] !== guess[i]) {
+                // Checks if letter is in the word but in the wrong position
                 if (tempWord.indexOf(guess[i]) > -1) {
-                    res += guess[i].toUpperCase();
+                    currentGuess[i] = guess[i].toUpperCase();
                     tempWord = tempWord.replace(guess[i], "-");
                     console.log(tempWord);
                 }
                 else {
-                    
-                    res += "-";
+                    currentGuess[i] = "-";
                 }
-
-            }
+            } 
         }
-        setResult(res);
-    };
+
+        setResult(currentGuess);
+    }
 
     function handleChange(event) {
         setGuess(event.target.value);
@@ -39,9 +42,9 @@ function Form(props) {
     }
 
     function handleSubmit(event) {
+        event.preventDefault();
         console.log("A guess was submitted: " + guess);
         compareLetters();
-        event.preventDefault();
     }
 
     return (
