ab563acb398f9a9eff75f262c568c4c5fd8ab87a
Amine <japnoo@gmail.com>
2022-03-22 01:15:47 +0100

    Added guess limit

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 function Form(props) {
     const [guess, setGuess] = useState("");
     const [guessList, setGuessList] = useState([]);
+    const [guessCount, setGuessCount] = useState(0);
+
     const [result, setResult] = useState("");
     const [isVictorious, setIsVictorious] = useState(false);
 
@@ -15,7 +17,7 @@ function Form(props) {
         if (guess.length !== word_len) {
             return;
         }
-
+        setGuessCount(guessCount + 1);
         let currentGuess = [word_len+1];
         let tempWord = props.word;
 
@@ -58,7 +60,10 @@ function Form(props) {
 
     function handleSubmit(event) {
         event.preventDefault();
-        if(!isVictorious) {
+        console.log(guessCount);
+        if(!isVictorious && guessCount < props.word.length + 1) {
+            console.log("in");
+
             compareLetters();
         }
     }
@@ -70,6 +75,8 @@ function Form(props) {
             </form>
             {guessList.map((guess, idx) => <div key={idx}>{guess}</div>)}
             {isVictorious ? <div>You won!</div> : ""}
+            {guessCount >= props.word.length + 1 ? <div>Out of guesses!</div> : ""}
+
         </div>
     );
 }
