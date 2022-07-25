562b856b9cbf9565b435a10e6515ae588f850097
Amine <japnoo@gmail.com>
2022-07-25 23:19:14 +0100

    added https (maybe)

--- api/server.js
+++ api/server.js
@@ -1,4 +1,6 @@
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 const express = require("express");
 const { dict } = require("./dict");
@@ -8,6 +10,13 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync("/etc/certs/key.pem"),
+    cert: fs.readFileSync("/etc/certs/cert.pem"),
+  },
+  app
+);
 // Function that gets a random word from the dictionary
 function getRandomWord(length) {
   let dictKeys = Object.keys(dict);
@@ -19,8 +28,9 @@ function getRandomWord(length) {
 }
 
 // This displays a message that the server is running and listening to a specified port
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
+httpsServer.listen(443, () => {
+  console.log("HTTPS Server running on port 443");
+});
 app.get("/word/random/:wordlen", (req, res) => {
   let wordLen = req.params.wordlen;
   let randomWord = getRandomWord(parseInt(wordLen, 10));
